594c5c733d9404d0e8701217947c055dfd1f7c58
Scott Ford <swford@ualberta.ca>
2021-11-19 12:37:53 -0700

    commented out tweepy section for testing

--- current/views.py
+++ current/views.py
@@ -17,14 +17,13 @@ def index(request):
     currency_names = []
     parameters = {
         'start': '1',
-        'limit': '10',
+        'limit': '5',
         'convert': 'CAD',
     }
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY,
     }
-
     session = Session()
     session.headers.update(headers)
     crypto_price_dict = {}
@@ -47,25 +46,24 @@ def index(request):
     auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
-    # run search query with cryptocurrency names to get the top 5 tweets for both
-    crypto_tweet_id_dict = {}
-    for name in currency_names:
-        popular_tweets = api.search_tweets(q=name, result_type='popular', tweet_mode='extended', count=5)
-        # get tweet id for embedding tweet in template
-        for tweet_data in popular_tweets:
-            pprint(tweet_data._json)
-            if tweet_data._json['id']:
-                url_to_embed = 'https://twitter.com/twitter/statuses/' + tweet_data._json['id_str']
-                oembed_tweet_url = api.get_oembed(url=url_to_embed)
-                crypto_tweet_id_dict[name] = oembed_tweet_url['html']
-                # print(oembed_tweet_url['html'])
+    # # run search queries with cryptocurrency names to get their embedded html code 
+    # crypto_tweet_id_dict = {}
+    # for name in currency_names:
+    #     popular_tweets = api.search_tweets(q=name, result_type='popular', tweet_mode='extended', count=1)
+    #     # get tweet id for embedding tweet in template
+    #     for tweet_data in popular_tweets:
+    #         pprint(tweet_data._json)
+    #         if tweet_data._json['id']:
+    #             url_to_embed = 'https://twitter.com/twitter/statuses/' + tweet_data._json['id_str']
+    #             oembed_tweet_url = api.get_oembed(url=url_to_embed)
+    #             crypto_tweet_id_dict[name] = oembed_tweet_url['html']
 
     context = {
         'crypto_price_dict': crypto_price_dict,
-        'crypto_tweet_id_dict': crypto_tweet_id_dict,
+        # 'crypto_tweet_id_dict': crypto_tweet_id_dict,
     }
 
     pprint(crypto_price_dict)
-    pprint(crypto_tweet_id_dict)
+    # pprint(crypto_tweet_id_dict)
 
     return render(request, 'current/index.html', context)
