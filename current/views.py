daef68c291f99976407f642a833f0b87d3226a35
Scott Ford <swford@ualberta.ca>
2021-11-18 17:10:51 -0700

    modified part of view function to get tweet oembed html

--- current/views.py
+++ current/views.py
@@ -6,12 +6,11 @@ from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import tweepy
 
-# index page with current top tweets, newsheadlines, crypto currency stocks
+# index page with current crypto currency stocks, and popular tweets about them
 def index(request):
     """
     main view for displaying current data: cryptocurrency stocks and tweets
     """
-
     # initialize coinmarketcap API access and get crypto currency names for search_tweet 'q' parameter
     CRYPTO_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
     COINMARKETCAP_API_KEY = os.environ['COINMARKETCAP_KEY']
@@ -21,7 +20,6 @@ def index(request):
         'limit': '10',
         'convert': 'CAD',
     }
-
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': COINMARKETCAP_API_KEY,
@@ -29,14 +27,14 @@ def index(request):
 
     session = Session()
     session.headers.update(headers)
-    currency_price_dict = {}
+    crypto_price_dict = {}
 
     try:
         response = session.get(CRYPTO_URL, params=parameters)
         data = json.loads(response.text)
         for entry in data['data']:
             currency_names.append(entry['name'])
-            currency_price_dict[entry['name']] = {'CAD_price': entry['quote']['CAD']['price']}  
+            crypto_price_dict[entry['name']] = entry['quote']['CAD']['price']
     except(ConnectionError, Timeout, TooManyRedirects) as e:
         pprint(e)
     
@@ -50,22 +48,24 @@ def index(request):
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
     # run search query with cryptocurrency names to get the top 5 tweets for both
+    crypto_tweet_id_dict = {}
     for name in currency_names:
-        tweet_ids = []
         popular_tweets = api.search_tweets(q=name, result_type='popular', tweet_mode='extended', count=5)
         # get tweet id for embedding tweet in template
         for tweet_data in popular_tweets:
+            pprint(tweet_data._json)
             if tweet_data._json['id']:
-                tweet_ids.append(tweet_data._json['id'])
-        # add tweet_ids to appropriate key in the currency_price_dict 
-        if name in currency_price_dict.keys():
-            currency_price_dict[name]['tweet_ids'] = tweet_ids
+                url_to_embed = 'https://twitter.com/twitter/statuses/' + tweet_data._json['id_str']
+                oembed_tweet_url = api.get_oembed(url=url_to_embed)
+                crypto_tweet_id_dict[name] = oembed_tweet_url['html']
+                # print(oembed_tweet_url['html'])
 
     context = {
-        'currency_price_dict': currency_price_dict,
-
+        'crypto_price_dict': crypto_price_dict,
+        'crypto_tweet_id_dict': crypto_tweet_id_dict,
     }
 
-    pprint(currency_price_dict)
+    pprint(crypto_price_dict)
+    pprint(crypto_tweet_id_dict)
 
-    return render(request, 'current/index.html')
+    return render(request, 'current/index.html', context)
